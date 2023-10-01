5c21a571a41152e2d11b76f0bb26f5c02f3ffdd5
OGATA Tetsuji <tetsuji.ogata@gmail.com>
2023-10-01 03:44:54 +0000

    add some feature to install.sh fr Codespaces

--- install.sh
+++ install.sh
@@ -2,9 +2,40 @@
 set -eu
 
 function main {
-    backup-dotfiles
+    if ! is-rcm-exist ; then
+        echo "rcm is not installed" >&2
+        return 1
+    fi
+    if ! is-codespaces ; then
+        echo "this script is only for codespaces" >&2
+        return 1
+    fi
+    backup-home-real-dotfiles
+    create-home-dotfiles-dir-symlink
     pwd # for debug
-    env RCRC=./rcrc rcup
+    # Codespaces の場合、シンボリックリンクではなく、-C でコピーを作成する
+    #   あと、-f で対話が発生しないようにする
+    # Codespaces の場合、~/.dotfiles 自体が揮発的になってしまうことと、
+    #   これ自体をシンボリックリンクにしてしまうと、シンボリックリンクが多重となってしまい混乱を生む可能性があるため
+    #   -C でコピーを作ることでもろもろ回避しようとしている
+    #   ドットファイルに変更があれば、再度 rcup を実行したり、
+    env RCRC=./rcrc rcup -v -f -C
+}
+
+function is-pwd-dotfiles-root {
+    if [ -f "./rcrc" ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+function is-rcm-exist {
+    if type "rcup" > /dev/null 2>&1; then
+        return 0
+    else
+        return 1
+    fi
 }
 
 function is-codespaces {
@@ -15,12 +46,29 @@ function is-codespaces {
     fi
 }
 
+function create-home-dotfiles-dir-symlink {
+    cloned_dotfiles_dir=$PWD
+    ln -v -s $cloned_dotfiles_dir "$HOME/.dotfiles"
+}
+
 # 既存のホームディレクトリにドットファイルがある場合、シンボリックでなければバックアップする
-function backup-dotfiles {
-  if [ -e "$HOME/$1" -a ! -L "$HOME/$1" ]; then
-    echo "backup $HOME/$1"
-    mv "$HOME/$1" "$HOME/$1.bak"
-  fi
+function backup-home-real-dotfiles {
+     find "$HOME" \
+        -maxdepth 1 \
+        -name '.??*' \
+        -not -name '.*.bak' \
+        -not -name .dotfiles \
+        -not -type l \
+    | while read dotfile ; do
+        backup_file="$dotfile.bak"
+        # すでにリアルの ~/.foo がある場合、コピーするものと差分がなければバックアップしない
+        if [ -f "$backup_file" ] && cmp -s "$dotfile" "$backup_file" ; then
+            echo "skip backup: $dotfile -> $backup_file"
+            continue
+        fi
+        echo "backup: $dotfile -> $backup_file"
+        cp -p "$dotfile" "$backup_file"
+    done
 }
 
 main "$@"
